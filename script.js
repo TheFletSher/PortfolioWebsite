8538546516ceef95ccb45025625c35041425cd38
TheFletSher <lfhsherson95@gmail.com>
2025-05-27 18:22:57 -0400

    drop down menu

--- script.js
+++ script.js
@@ -1,5 +1,15 @@
 const toggleButton = document.getElementById("darkModeToggle");
+const dropdownButton = document.getElementById("dropdownButton");
+const dropdownMenu = document.getElementById("dropdownMenu");
 
 toggleButton.addEventListener("change", () => {
     document.body.classList.toggle("dark-mode");
+});
+
+dropdownButton.addEventListener("click", () => {
+    dropdownMenu.classList.toggle("hidden"); 
+});
+
+dropdownButton.addEventListener("click", () => {
+    dropdownMenu.classList.toggle("show");
 });
